627e9f3ba4cca18d37d1605a096bf11c6204c1fe
Ali Fadda <silvertechguy@gmail.com>
2020-10-12 23:46:54 +0200

    add two projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -30,13 +30,36 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.png',
+    img: 'woofer.png',
     title: 'woofer',
     info:
       "Woofer is a full-stack web application using the (PERN) Stack. It's a twitter-like application that allows you to post woofs (tweets), see woof details such as comments and likes, get notifications, see user profile, edit profile, like and unlike, comment, and do the basic CRUD operations. Used React to make the front end. Built backend with Node and Express where we have a restful API server. Created session management with authenticated and protected routes using Redis and JWT. Stored the application data securely in the PostgreSQL database. Used Docker and Docker Compose to make development processes easier.Integrated Nexmo API to send SMS whenever someone register.",
-    info2: '',
+    info2:
+      'Signup / Login - Notifications 🔔 - View Profile - Upload Photo - Edit Profile - New Woof (tweet) - Like / UnLike - Comment - View Woof.',
     url: 'https://woofer-official.herokuapp.com/',
     repo: 'https://github.com/silvertechguy/woofer', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'shipped.png',
+    title: 'shipped',
+    info:
+      'Shipped is an eCommerce platform built with MERN stack and Redux. Used React and Bootstrap to make the front end. Built the backend API server architecture with Node and Express. Added admin product and order management. Stored the application data in the NoSQL MongoDB database. Integrated PayPal for the payment method process.',
+    info2:
+      'Full-featured shopping cart - Product reviews and ratings - Top products carousel- Product pagination - Product search feature - User profile with orders - Admin product management - Admin user management - Admin Order details page - Mark orders as delivered option - Checkout process (shipping, payment method, etc) - PayPal / credit card integration - Database seeder (products & users).',
+    url: 'https://shipped-official.herokuapp.com/',
+    repo: 'https://github.com/silvertechguy/shipped', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'ppass.png',
+    title: 'ppass',
+    info:
+      'ppass is a secure way for you to check if your password has ever been hacked in a data breach. ppass Password checker built with Node, Express, TypeScript, React, and Redis.',
+    info2:
+      "Passwords get leaked all the time. We've heard about data breaches. Facebook has been hacked with data breaches. You do not want to send your password over the internet because it's being transferred to a server somewhere in the world. It's traveling through the internet wires and somebody could intercept it. haveibeenpwned.com gives us a password API to check our password if it's ever been hacked. I do not want to send my password to this API. The best security is to trust no one. Here is what I did. I sent a sha1 hashed version of my password. I only give him the first 5 characters of my hashed password. The API is going to look in its database of all these passwords and pick all the hashed passwords that have the first 5 characters that match our password. With the response data, I can now check my full hash to see if the password has ever been hacked. The API is never going to know our full hashed password and therefore never be able to guess our password.",
+    url: 'https://ppass-official.herokuapp.com/',
+    repo: 'https://github.com/silvertechguy/ppass', // if no repo, the button will not show up
   },
 ];
 
@@ -58,7 +81,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: 'https:linkedin.com/in/silvertechguy',
+      url: 'https://linkedin.com/in/silvertechguy',
     },
     {
       id: nanoid(),
